6c879844734370c96c08cce87437ade2a4113c95
sustachio <88939526+sustachio@users.noreply.github.com>
2023-04-18 22:12:23 +0000

    Refactor: move get_identifiers into main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from random import random, randint, choice
-from html_reader import get_identifiers
+from random import randint, choice
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
 
+def get_identifiers(file):
+    with open(file, "r") as f:
+        soup = BeautifulSoup(f.read(), "html.parser")
+        body = soup.find("body")
+    
+    classes = set([
+            class_ 
+            for element in body.find_all(class_=True)
+            for class_ in element["class"]
+    ])
+
+    ids = set([
+            element["id"] 
+            for element in body.find_all(id=True)
+    ])
+
+    tags = set([
+            element.name 
+            for element in body.find_all()
+    ])
+
+    return (["."+class_ for class_ in classes] +
+            ["#"+id for id in ids] + 
+            list(tags) +
+            ["body"])
+    
 app = Flask(__name__)
 
 PROPRETIES_RANGE = (2,4)
