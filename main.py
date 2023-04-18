66abf7572b2c46e1cb2ee34661670a235cf90ab9
sustachio <88939526+sustachio@users.noreply.github.com>
2023-04-18 19:36:02 +0000

    Feature: add some more propreties

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 PROPRETIES_RANGE = (2,4)
+
 COLOR = ["black", "silver", "gray", "white", "maroon", "red", "purple", "fuchsia", "green", "lime", "olive", "yellow", "navy", "blue", "teal", "aqua"]
 
 identifiers = get_identifiers("templates/index.html")
@@ -17,6 +18,22 @@ properties = [
     {
         "name": "color",
         "arguments": [COLOR,]
+    },
+    {
+        "name": "float",
+        "arguments": [["left", "right"]]
+    },
+    {
+        "name": "border",
+        "arguments": [
+            ["thin", "medium", "thick"],
+            ["dotted", "dashed", "solid", "double", "groove", "ridge", "inset", "outset"],
+            COLOR
+        ]
+    },
+    {
+        "name": "display",
+        "arguments": [["inline", "block", "inline-block", "table", "flex"]]
     }
 ]
 
