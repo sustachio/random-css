a3b29c8cf13edc66fcf290faca9e5261775a1442
sustachio <88939526+sustachio@users.noreply.github.com>
2023-04-18 19:36:28 +0000

    Fix: only make css for body and children

--- html_reader.py
+++ html_reader.py
@@ -3,23 +3,25 @@ from bs4 import BeautifulSoup
 def get_identifiers(file):
     with open(file, "r") as f:
         soup = BeautifulSoup(f.read(), "html.parser")
+        body = soup.find("body")
     
     classes = set([
             class_ 
-            for element in soup.find_all(class_=True)
+            for element in body.find_all(class_=True)
             for class_ in element["class"]
     ])
 
     ids = set([
             element["id"] 
-            for element in soup.find_all(id=True)
+            for element in body.find_all(id=True)
     ])
 
     tags = set([
             element.name 
-            for element in soup.find_all()
+            for element in body.find_all()
     ])
 
     return (["."+class_ for class_ in classes] +
             ["#"+id for id in ids] + 
-            list(tags))
+            list(tags) +
+            ["body"])
